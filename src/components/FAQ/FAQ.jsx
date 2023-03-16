b750a663bf96fbd4f252bd6a3016b538a71be4f2
MdSalman2022 <salman15-4523@diu.edu.bd>
2023-03-16 12:53:50 +0600

    faq section updated

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -23,13 +23,35 @@ const FAQ = () => {
                       <span onClick={() => setAccordionTwo(!accordionTwo)} className='cursor-pointer select-none flex items-center justify-between py-5 border-y gap-10 bg-base-100 z-50'><p>What thing that i should prepare before starting?</p><div className='relative'><FaMinus className={`absolute right-3 -top-2 transition-all duration-300 ${accordionTwo ? 'rotate-0' : 'rotate-90'} p-1 text-xl bg-transparent text-base-100 rounded-full z-50`} /><FaMinus className={minusStyle}  /></div></span> 
                       
 
-                      <div className={`bg-secondary overflow-hidden transition-all duration-300 ${accordionTwo ? "h-auto" : "h-0"}`}><div className={`p-4 transform transition-transform duration-300 ${accordionTwo ? 'translate-y-0' : '-translate-y-20'}`}><p>To signup click on Try it now! button.</p></div></div>
+                      <div className={`bg-secondary overflow-hidden transition-all duration-300 ${accordionTwo ? "h-auto" : "h-0"}`}><div className={`p-4 transform transition-transform duration-300 ${accordionTwo ? 'translate-y-0' : '-translate-y-20'}`}><div>
+                      <p>Before starting a marketing campaign with a marketing agency, there are a few things that you should prepare:</p>
+
+                          <ul className='list-decimal list-inside'>
+                            <li>Clearly define your goals and objectives for the campaign.</li>
+                            <li>Identify your target audience and understand their needs and preferences.</li>
+                            <li>Gather and analyze data on your current market position, competitors, and industry trends.</li>
+                            <li>Determine your budget for the campaign and establish metrics for measuring success.</li>
+                            <li>Have a clear understanding of your brand identity and messaging.</li>
+                            <li>By preparing these things in advance, you can help ensure that your marketing campaign is effective and aligned with your overall business objectives.</li>
+                        </ul>
+                      </div></div></div>
 
 
                       <span onClick={() => setAccordionThree(!accordionThree)} className='cursor-pointer select-none flex items-center justify-between py-5 border-y gap-10 bg-base-100 z-50'><p>Does my company need help for marketing advices?</p><div className='relative'><FaMinus className={`absolute right-3 -top-2 transition-all duration-300 ${accordionThree ? 'rotate-0' : 'rotate-90'} p-1 text-xl bg-transparent text-base-100 rounded-full z-50`} /><FaMinus className={minusStyle}  /></div></span> 
                       
 
-                      <div className={`bg-secondary overflow-hidden transition-all duration-300 ${accordionThree ? "h-auto" : "h-0"}`}><div className={`p-4 transform transition-transform duration-300 ${accordionThree ? 'translate-y-0' : '-translate-y-20'}`}><p>To signup click on Try it now! button.</p></div></div>
+                      <div className={`bg-secondary overflow-hidden transition-all duration-300 ${accordionThree ? "h-auto" : "h-0"}`}><div className={`p-4 transform transition-transform duration-300 ${accordionThree ? 'translate-y-0' : '-translate-y-20'}`}><div>
+                      <p>If your company is looking to improve its marketing efforts, a marketing agency can be a valuable partner. Marketing agencies can provide expert advice and guidance on a wide range of marketing activities, including branding, digital marketing, social media, content marketing, and more.</p>
+                        <p>Some signs that your company may benefit from the help of a marketing agency include:</p>
+                          <ul className='list-decimal list-inside'>
+                            <li>Your current marketing efforts are not generating the desired results.</li>
+                            <li>Your marketing team lacks the expertise or resources to execute a comprehensive marketing strategy.</li>
+                            <li>You are looking to expand your marketing efforts into new channels or markets.</li>
+                            <li>You need help developing a cohesive brand identity and messaging.</li>
+                          </ul>
+
+                      
+                      </div></div></div>
  
                   </div> 
                   <div className='flex flex-col justify-between gap-5'>
